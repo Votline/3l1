fde951cd2faf98f3f639fa4ca975313bee84c634
Votline <quogg2@gmail.com>
2025-11-08 18:33:53 +0300

    Added rpc: extract jwt data, protos v0.0.5

--- protos/generated-user/user-service.pb.go
+++ protos/generated-user/user-service.pb.go
@@ -325,6 +325,102 @@ func (x *LogRes) GetToken() string {
 	return ""
 }
 
+type ExtJWTDataReq struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Token         string                 `protobuf:"bytes,1,opt,name=token,proto3" json:"token,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *ExtJWTDataReq) Reset() {
+	*x = ExtJWTDataReq{}
+	mi := &file_user_service_proto_msgTypes[6]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ExtJWTDataReq) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ExtJWTDataReq) ProtoMessage() {}
+
+func (x *ExtJWTDataReq) ProtoReflect() protoreflect.Message {
+	mi := &file_user_service_proto_msgTypes[6]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ExtJWTDataReq.ProtoReflect.Descriptor instead.
+func (*ExtJWTDataReq) Descriptor() ([]byte, []int) {
+	return file_user_service_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *ExtJWTDataReq) GetToken() string {
+	if x != nil {
+		return x.Token
+	}
+	return ""
+}
+
+type ExtJWTDataRes struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	UserId        string                 `protobuf:"bytes,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	Role          string                 `protobuf:"bytes,2,opt,name=role,proto3" json:"role,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *ExtJWTDataRes) Reset() {
+	*x = ExtJWTDataRes{}
+	mi := &file_user_service_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *ExtJWTDataRes) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ExtJWTDataRes) ProtoMessage() {}
+
+func (x *ExtJWTDataRes) ProtoReflect() protoreflect.Message {
+	mi := &file_user_service_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ExtJWTDataRes.ProtoReflect.Descriptor instead.
+func (*ExtJWTDataRes) Descriptor() ([]byte, []int) {
+	return file_user_service_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *ExtJWTDataRes) GetUserId() string {
+	if x != nil {
+		return x.UserId
+	}
+	return ""
+}
+
+func (x *ExtJWTDataRes) GetRole() string {
+	if x != nil {
+		return x.Role
+	}
+	return ""
+}
+
 var File_user_service_proto protoreflect.FileDescriptor
 
 const file_user_service_proto_rawDesc = "" +
@@ -346,11 +442,18 @@ const file_user_service_proto_rawDesc = "" +
 	"\x05email\x18\x02 \x01(\tR\x05email\x12\x1a\n" +
 	"\bpassword\x18\x03 \x01(\tR\bpassword\"\x1e\n" +
 	"\x06LogRes\x12\x14\n" +
-	"\x05token\x18\x01 \x01(\tR\x05token2\x8b\x01\n" +
+	"\x05token\x18\x01 \x01(\tR\x05token\"%\n" +
+	"\rExtJWTDataReq\x12\x14\n" +
+	"\x05token\x18\x01 \x01(\tR\x05token\"<\n" +
+	"\rExtJWTDataRes\x12\x17\n" +
+	"\auser_id\x18\x01 \x01(\tR\x06userId\x12\x12\n" +
+	"\x04role\x18\x02 \x01(\tR\x04role2\xc5\x01\n" +
 	"\vUserService\x12*\n" +
 	"\bHashPswd\x12\x0e.users.HashReq\x1a\x0e.users.HashRes\x12'\n" +
 	"\aRegUser\x12\r.users.RegReq\x1a\r.users.RegRes\x12'\n" +
-	"\aLogUser\x12\r.users.LogReq\x1a\r.users.LogResB\x10Z\x0e./;userserviceb\x06proto3"
+	"\aLogUser\x12\r.users.LogReq\x1a\r.users.LogRes\x128\n" +
+	"\n" +
+	"ExtJWTData\x12\x14.users.ExtJWTDataReq\x1a\x14.users.ExtJWTDataResB\x10Z\x0e./;userserviceb\x06proto3"
 
 var (
 	file_user_service_proto_rawDescOnce sync.Once
@@ -364,24 +467,28 @@ func file_user_service_proto_rawDescGZIP() []byte {
 	return file_user_service_proto_rawDescData
 }
 
-var file_user_service_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_user_service_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_user_service_proto_goTypes = []any{
-	(*HashReq)(nil), // 0: users.HashReq
-	(*HashRes)(nil), // 1: users.HashRes
-	(*RegReq)(nil),  // 2: users.RegReq
-	(*RegRes)(nil),  // 3: users.RegRes
-	(*LogReq)(nil),  // 4: users.LogReq
-	(*LogRes)(nil),  // 5: users.LogRes
+	(*HashReq)(nil),       // 0: users.HashReq
+	(*HashRes)(nil),       // 1: users.HashRes
+	(*RegReq)(nil),        // 2: users.RegReq
+	(*RegRes)(nil),        // 3: users.RegRes
+	(*LogReq)(nil),        // 4: users.LogReq
+	(*LogRes)(nil),        // 5: users.LogRes
+	(*ExtJWTDataReq)(nil), // 6: users.ExtJWTDataReq
+	(*ExtJWTDataRes)(nil), // 7: users.ExtJWTDataRes
 }
 var file_user_service_proto_depIdxs = []int32{
 	0, // 0: users.UserService.HashPswd:input_type -> users.HashReq
 	2, // 1: users.UserService.RegUser:input_type -> users.RegReq
 	4, // 2: users.UserService.LogUser:input_type -> users.LogReq
-	1, // 3: users.UserService.HashPswd:output_type -> users.HashRes
-	3, // 4: users.UserService.RegUser:output_type -> users.RegRes
-	5, // 5: users.UserService.LogUser:output_type -> users.LogRes
-	3, // [3:6] is the sub-list for method output_type
-	0, // [0:3] is the sub-list for method input_type
+	6, // 3: users.UserService.ExtJWTData:input_type -> users.ExtJWTDataReq
+	1, // 4: users.UserService.HashPswd:output_type -> users.HashRes
+	3, // 5: users.UserService.RegUser:output_type -> users.RegRes
+	5, // 6: users.UserService.LogUser:output_type -> users.LogRes
+	7, // 7: users.UserService.ExtJWTData:output_type -> users.ExtJWTDataRes
+	4, // [4:8] is the sub-list for method output_type
+	0, // [0:4] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -398,7 +505,7 @@ func file_user_service_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_user_service_proto_rawDesc), len(file_user_service_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- protos/generated-user/user-service_grpc.pb.go
+++ protos/generated-user/user-service_grpc.pb.go
@@ -19,9 +19,10 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	UserService_HashPswd_FullMethodName = "/users.UserService/HashPswd"
-	UserService_RegUser_FullMethodName  = "/users.UserService/RegUser"
-	UserService_LogUser_FullMethodName  = "/users.UserService/LogUser"
+	UserService_HashPswd_FullMethodName   = "/users.UserService/HashPswd"
+	UserService_RegUser_FullMethodName    = "/users.UserService/RegUser"
+	UserService_LogUser_FullMethodName    = "/users.UserService/LogUser"
+	UserService_ExtJWTData_FullMethodName = "/users.UserService/ExtJWTData"
 )
 
 // UserServiceClient is the client API for UserService service.
@@ -31,6 +32,7 @@ type UserServiceClient interface {
 	HashPswd(ctx context.Context, in *HashReq, opts ...grpc.CallOption) (*HashRes, error)
 	RegUser(ctx context.Context, in *RegReq, opts ...grpc.CallOption) (*RegRes, error)
 	LogUser(ctx context.Context, in *LogReq, opts ...grpc.CallOption) (*LogRes, error)
+	ExtJWTData(ctx context.Context, in *ExtJWTDataReq, opts ...grpc.CallOption) (*ExtJWTDataRes, error)
 }
 
 type userServiceClient struct {
@@ -71,6 +73,16 @@ func (c *userServiceClient) LogUser(ctx context.Context, in *LogReq, opts ...grp
 	return out, nil
 }
 
+func (c *userServiceClient) ExtJWTData(ctx context.Context, in *ExtJWTDataReq, opts ...grpc.CallOption) (*ExtJWTDataRes, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ExtJWTDataRes)
+	err := c.cc.Invoke(ctx, UserService_ExtJWTData_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // UserServiceServer is the server API for UserService service.
 // All implementations must embed UnimplementedUserServiceServer
 // for forward compatibility.
@@ -78,6 +90,7 @@ type UserServiceServer interface {
 	HashPswd(context.Context, *HashReq) (*HashRes, error)
 	RegUser(context.Context, *RegReq) (*RegRes, error)
 	LogUser(context.Context, *LogReq) (*LogRes, error)
+	ExtJWTData(context.Context, *ExtJWTDataReq) (*ExtJWTDataRes, error)
 	mustEmbedUnimplementedUserServiceServer()
 }
 
@@ -96,6 +109,9 @@ func (UnimplementedUserServiceServer) RegUser(context.Context, *RegReq) (*RegRes
 }
 func (UnimplementedUserServiceServer) LogUser(context.Context, *LogReq) (*LogRes, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LogUser not implemented")
+}
+func (UnimplementedUserServiceServer) ExtJWTData(context.Context, *ExtJWTDataReq) (*ExtJWTDataRes, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ExtJWTData not implemented")
 }
 func (UnimplementedUserServiceServer) mustEmbedUnimplementedUserServiceServer() {}
 func (UnimplementedUserServiceServer) testEmbeddedByValue()                     {}
@@ -172,6 +188,24 @@ func _UserService_LogUser_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _UserService_ExtJWTData_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ExtJWTDataReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserServiceServer).ExtJWTData(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: UserService_ExtJWTData_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserServiceServer).ExtJWTData(ctx, req.(*ExtJWTDataReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // UserService_ServiceDesc is the grpc.ServiceDesc for UserService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -190,6 +224,10 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LogUser",
 			Handler:    _UserService_LogUser_Handler,
+		},
+		{
+			MethodName: "ExtJWTData",
+			Handler:    _UserService_ExtJWTData_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
